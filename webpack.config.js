eb03f242b9fc6e42b894dc0dbc9afdcb355e3524
Joacim Pettersson <joacimpettersson91@gmail.com>
2021-10-13 13:35:39 +0200

    (build)

--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,50 @@
-"use strict";
+'use strict';
 const path = require('path');
 
 module.exports = {
-	devtool: false,
-	mode: "production",
-	entry: "./src/app.tsx",
-	output: {
-		path: path.join(__dirname, '/public'),
-		filename: "bundle.js"
-	},
-	module: {
-		rules: [
-			{
-				test: /\.tsx?$/,
-				loader: "ts-loader"
-			},
-			{
-				test: /\.css$/,
-				use: [
-					{
-						loader: "style-loader"
-					},
-					{
-						loader: "css-loader",
-						options: {
-							sourceMap: true,
-						}
-					}
-				]
-			}
-		]
-	},
-	resolve: {
-		extensions: [
-			".ts",
-			".tsx",
-			".js",
-			".jsx",
-			".json",
-			".css",
-		]
-	},
-	devServer: {
-		historyApiFallback: true,
-		contentBase: path.join(__dirname, 'public'),
-		compress: true,
-		port: 80
-	}
-}
+    devtool: false,
+    mode: 'production',
+    entry: './src/app.tsx',
+    output: {
+        path: path.join(__dirname, '/public'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: 'ts-loader'
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: 'style-loader'
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: true,
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    resolve: {
+        extensions: [
+            '.ts',
+            '.tsx',
+            '.js',
+            '.jsx',
+            '.json',
+            '.css',
+        ]
+    },
+    devServer: {
+        historyApiFallback: true,
+        static: path.join(__dirname, 'public'),
+        compress: true,
+        port: 80
+    }
+};
